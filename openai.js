27bec19ab0213b2ed16613363a4dd33598b1b447
henry.sweat <hi@henrysweat.com>
2023-12-06 13:38:38 -0500

    add error hangling to openai instantiation

--- openai.js
+++ openai.js
@@ -2,4 +2,12 @@ import 'dotenv/config';
 
 import OpenAI from 'openai';
 
-export const openai = new OpenAI();
+export function createOpenAIInstance() {
+  try {
+    const openai = new OpenAI();
+    return openai;
+  } catch (error) {
+    console.error('Failed to create OpenAI instance: ', error);
+    return null;
+  }
+}
